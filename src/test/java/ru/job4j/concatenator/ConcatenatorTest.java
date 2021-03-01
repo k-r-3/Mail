c1258566941e7d1ef2bf468cef59ed07935f4e9a
k-r-3 <rke19922802@gmail.com>
2021-03-02 00:44:08 +0500

    Почта. [#184458]

--- src/test/java/ru/job4j/concatenator/ConcatenatorTest.java
+++ src/test/java/ru/job4j/concatenator/ConcatenatorTest.java
@@ -25,7 +25,7 @@ public class ConcatenatorTest {
         UserInput input = new UserInput();
         Concatenator c = new Concatenator(cUser);
         c.operation(input);
-        assertThat(c.getMap().size(), is(2));
+        assertThat(c.getMap().size(), is(3));
     }
 
     @Test
